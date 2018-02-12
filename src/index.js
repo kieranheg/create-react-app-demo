83effef84445626268a96122fb7735cb0c440f51
Kieran Hegarty <kieranheg@users.noreply.github.com>
2018-02-12 15:26:06 +0000

    Added route passing props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+import "./index.css";
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <Main/>,
+    document.getElementById("root")
+);
